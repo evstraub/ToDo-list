b43c3acc9ccbcbd03da18b7b6daa064cb6aadb46
Ethan Straub <evstraub@gmail.com>
2022-12-22 11:25:58 -0500

    added fns and ui form clearing

---
+++ src/dom-manip.js
@@ -0,0 +1,58 @@
+import { todosFunc } from './todosBlankLoad.js';
+
+export function displayDefaultProject() {
+  // DOM for the default project load
+  const projectsInfoDiv = document.createElement('div');
+  projectsInfoDiv.textContent = todosFunc().projectTitle;
+  // contentDiv.appendChild(projectsInfoDiv);
+}
+
+export function displayTheForm() {
+  document.getElementById('add-todo-form').style.display = '';
+}
+
+export function addItemToCheckList() {
+  const addItem = document.getElementById('add-to-checklist').value;
+
+  // Run check to see if the input box is empty and button was clicked
+  // If so, return out of this function gracefully - do nothing
+  // If not, apply contents to new li in DOM
+  if (addItem !== '') {
+    const ul = document.querySelector('.todo-ul');
+    const li = document.createElement('li');
+    li.textContent = addItem;
+    const span = document.createElement('span');
+    span.className = 'remove-checklist-item';
+    const removeIcon = document.createTextNode('\u00D7');
+    li.appendChild(span);
+    span.appendChild(removeIcon);
+    ul.appendChild(li);
+    document.getElementById('add-to-checklist').value = '';
+
+    // DOM check for existing check list items already present
+    if (document.querySelectorAll('li').length > 0) {
+      console.log(
+        'INSIDE MODULE IF....,',
+        document.querySelectorAll('li').length
+      );
+      const nodeListCheckList = document.querySelectorAll('li');
+      console.log(nodeListCheckList);
+
+      // DOM to bind click event to each node in nodelist and remove node if clicked
+      nodeListCheckList.forEach((checkListItem) => {
+        checkListItem.addEventListener('click', () => {
+          checkListItem.remove();
+        });
+      });
+    }
+  } else;
+}
+
+export function clearForm() {
+  const nodeListCheckList = document.querySelectorAll('li');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < nodeListCheckList.length; i++) {
+    nodeListCheckList[i].remove();
+  }
+  document.getElementById('add-todo').reset();
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,56 @@
 import './style.css';
 import { createToDo } from './create-to-do.js';
 import { todosFunc } from './todosBlankLoad.js';
-import { initialdomManip } from './initial-dom-manip.js';
+import {
+  displayDefaultProject,
+  displayTheForm,
+  addItemToCheckList,
+  clearForm,
+} from './dom-manip.js';
 
 // call todosFunc on first land
 todosFunc();
 
-// call dom manip ui module to control ui
-initialdomManip();
+// Call DOM Manipulation module to load default project
+// displayDefaultProject();
 
-// TODO' click event module HERE for todo and project creations
+// Click events module
+const clickEventsModule = (function () {
+  // Click event for displaying the form
+  const addNewToDo = document.querySelector('.add-todo-button');
+  addNewToDo.addEventListener('click', displayTheForm);
+
+  // Click event for adding an item to the checklist on the form
+  const addToChecklist = document.querySelector('.add-to-checklist');
+  addToChecklist.addEventListener('click', addItemToCheckList);
+
+  // Click event to clear the form
+  const clearButton = document.querySelector('.reset-button');
+  clearButton.addEventListener('click', clearForm);
+
+  // Click event to submit a new todo form to project
+  const submitButton = document.querySelector('.submit-button');
+  submitButton.addEventListener('click', createToDo);
+})();
 
 // call create-to-do.js module file and appl some objects/properties(this will eventually be replaced by ui)
-const myToDo = createToDo(
-  'christmas presents',
-  'for emma',
-  '02/25/2001',
-  'high',
-  'card',
-  'money',
-  'book'
-);
-const myToDo2 = createToDo(
-  'christmas presents',
-  'for lane',
-  '02/25/2001',
-  'high',
-  'ticket',
-  'dollars',
-  'video games'
-);
-console.log('show me properties on my ToDo from index.js.....', myToDo);
-console.log('show me properties on my ToDo2 from index.js.....', myToDo2);
+// const myToDo = createToDo(
+//   'christmas presents',
+//   'for emma',
+//   '02/25/2001',
+//   'high',
+//   'card',
+//   'money',
+//   'book'
+// );
+// const myToDo2 = createToDo(
+//   'christmas presents',
+//   'for lane',
+//   '02/25/2001',
+//   'high',
+//   'ticket',
+//   'dollars',
+//   'video games'
+// );
+// console.log('show me properties on my ToDo from index.js.....', myToDo);
+// console.log('show me properties on my ToDo2 from index.js.....', myToDo2);
