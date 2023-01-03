fa06ea3ade7952845fbfcfec4f43196747078e1a
Ethan Straub <evstraub@gmail.com>
2023-01-03 16:12:21 -0500

    added modal and form

--- src/ToDo-List.js
+++ src/ToDo-List.js
@@ -1,12 +1,24 @@
+// import { setDateField } from './date-input';
+
 export function ToDoList() {
   const addToDoButton = document.getElementById('addToDo');
   const toDoContainer = document.getElementById('toDoContainer');
   const inputField = document.getElementById('inputField');
+  const modal = document.querySelector('#modal');
+  const openModal = document.querySelector('.open-button');
+  // const closeModal = document.querySelector('.close-button');
 
+  openModal.addEventListener('click', () => {
+    modal.showModal();
+  });
+  // closeModal.addEventListener('click', () => {
+  // modal.close();
+  // });
   addToDoButton.addEventListener('click', () => {
-    const paragraph = document.createElement('p');
+    const paragraph = document.createElement('div');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
+
     toDoContainer.appendChild(paragraph);
     inputField.value = '';
     paragraph.addEventListener('click', () => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './style.css';
+
 import { ToDoList } from './ToDo-List.js';
+
 ToDoList();
